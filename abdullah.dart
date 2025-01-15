e41668ec953d4412bd94e3676beabb41dba9f2dc
abdullah-604 <a333d7@gmail.com>
2025-01-15 19:28:31 +0300

    mobile&pervasive.com

---
+++ abdullah.dart
@@ -0,0 +1,9 @@
+void main(){
+  String myDetails = '''
+MY NAME : Abdullah saleh almasabi.
+rigistration Number : 52222209.
+Specialixation : Infromation tecnology.
+Level : Level 3;
+''';
+print(myDetails);
+}
